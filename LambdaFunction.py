a7eedfb752b891bcfdde6a7f5cdb7c4883f30fd0
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-07-13 21:35:58 +0530

    lambda

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -17,3 +17,5 @@ print(a)
 List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10]
 print(list(filter(lambda x: x % 2 == 0, List)))
 print(set(filter(lambda x: x % 2 == 0, List)))
+print(list(map(lambda x: x + 2, List)))
+print(list(map(lambda x: x % 2 == 0, List)))
