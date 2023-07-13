3c4f9c0d4afc42f24107bd3f0888f0aaa551bf8c
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-07-13 19:48:44 +0530

    lambda

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -1,14 +1,19 @@
-# Lambda function ao anonymous function
+# Lambda function an anonymous function
 # it is one line function
 
-# minus = lambda x, y: x - y
-# print(minus(5,7))
+minus = lambda x, y: x - y
+print(minus(5, 7))
+
 
 def firstindex(a):
     return a[1]
 
 
 a = [[1, 14], [5, 6], [8, 23]]
-# a.sort(key=firstindex) # it takes the keys length
+a.sort(key=firstindex)  # it takes the keys length
 a.sort(key=lambda x: x[1])
 print(a)
+
+List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10]
+print(list(filter(lambda x: x % 2 == 0, List)))
+print(set(filter(lambda x: x % 2 == 0, List)))
