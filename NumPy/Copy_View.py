28064cd76793be20f51d7002ec613260c63959a3
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-07-26 10:03:03 +0530

    np

---
+++ NumPy/Copy_View.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+arr = np.array([1, 5, 7, 9, 8, 6, 4])
+x = arr.copy()
+# print(x)
+x[4] = 10
+# print(x)
+# print(arr)
+
+y = arr.view()
+# print(y)
+y[4] = 2
+# print(y)
+# print(arr)
+
+
+# " I t e r a t i o n "
+arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+# for i in arr1:  # this returns the array
+    # print(i)
+# for i in arr1:
+    # for j in i:  # this returns the elements
+        # print(j)  # if the dimension increases than complexity will increase so below given is the good way to print
+                    # elements
+# for i in np.nditer(arr1):
+#     print(i)
+for i in np.nditer(arr1[:1]):  # indexing
+    print(i)
