21ce88ca2182ecaab81cdab87fb9976587dd00a6
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-08-10 17:44:23 +0530

    dc

--- Dictionary.py
+++ Dictionary.py
@@ -1,5 +1,15 @@
-search = input("enter the word: ")
+# search = input("enter the word: ")
 
-dict = {"bhavin": "hi ma name is bhavin i am a programmer", "beguiling": "charming and attractive"}
+# dict = {"bhavin": "hi ma name is bhavin i am a programmer", "beguiling": "charming and attractive"}
 
-print(dict[search])
+# print(dict[search])
+
+
+word = 'brontosaurus'
+d = dict()
+for c in word:
+    if c not in d:
+        d[c] = 1
+else:
+    d[c] += 1
+print(d)
