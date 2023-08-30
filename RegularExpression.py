493cb104fa1f75d6c8b41ec9be7e18e25b54843e
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-08-30 21:47:36 +0530

    re

--- RegularExpression.py
+++ RegularExpression.py
@@ -2,22 +2,25 @@ import re
 
 # " Data Cleaning "
 stri = ''' hi my name is bhavin, my contact number is +91-8780392112, you can call me anytime and in emergency you can
- contact me on number +91-7600270056'''
-# number = re.findall('918780392112', stri)
+ contact me on number +91-7600270056. I am doing data structures course the language used is JAVA'''
+# number = re.findall('91-8780392112', stri)
 # print(number)
 # number = re.findall('\d', stri)
 # print(number)  # prints the all single digit
 # number = re.findall('\d+', stri)
 # print(number)  # prints all the digits
+# all = re.findall("\??i", stri)
+# print(all)
 
 # so the pattern we can find is first two digit country code and rest the number
-number = re.findall('\d{2}-\d{10}', stri)  # ' \d+ ' : can be used in authentication
+# number = re.findall('\d{2}-\d{10}', stri)  # ' \d+ ' : can be used in authentication
 # print(number)
 
 # for string
 strii = "hi my name is bhavin, i am a programmer, contact me on number +91-7600270056"
 serstr = re.search('^is', strii)  # we can confirm the string starting with
-# serstr = re.search('is', strii)  # we can confirm the string starting with
+# print(serstr)
+# serstr = re.search('is', strii)
 # if serstr:  # if string with space then space is also going to be counted
 #     print("matched")
 # else:
@@ -34,8 +37,8 @@ strm = re.match("l\w+", str1)  # '\w':means the single character
 
 
 # " Split "
-# spli = re.split('\s', str1)
-spli = re.split('\s', str1, 5)
+spli = re.split('\s', str1)
+# spli = re.split('\s', str1, 5)
 # print(spli)
 
 
@@ -50,13 +53,15 @@ str3 = "mumbai is the financial capital of india"
 meta = re.findall("[a-g]", str3)  # finding in the range
 # print(meta)
 end = re.findall("india$", str3)
+end1 = re.findall("\Ais", str3)
 # print(end)
+# print(end1)
 
 
 # " Dot " it is used to join the search
 st = "today is 31st july, today is monday"
 dot = re.search("^today.+monday$", st)  # '*' is used to match single or whole word occurrence and also prints
-print(dot)                                      #  the stuff that does not match
+# print(dot)                                      #  the stuff that does not match
 # find = re.findall("t*", st)
 find = re.findall("t+", st)
 # find = re.search("t", st)
